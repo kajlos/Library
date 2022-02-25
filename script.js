d1b799337fc45b7139332e72a8ac11c2f3122c90
Kajetan <kajetan.loska@op.pl>
2022-02-25 14:20:22 +0100

    done

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ const submitButton = document.querySelector('[data-submit-button]');
 const card = document.querySelector('[data-card]');
 const overlay = document.querySelector('[data-overlay]');
 const form = document.getElementById('form');
-const bookGrid = document.querySelector('[data-book-grid]')
+const bookGrid = document.querySelector('[data-book-grid]');
+const errorfield = document.querySelector('[data-error]')
 console.log(bookGrid);
 addBookButton.addEventListener('click',()=>{
     let author = document.getElementById('author');
     let title = document.getElementById('title');
     let pages = document.getElementById('pages');
     let read = document.getElementById('read');
+    errorfield.textContent="";
     author.value = '';
     title.value = '';
     pages.value = '';
@@ -20,33 +22,45 @@ addBookButton.addEventListener('click',()=>{
 let Books = [];
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
+
     let author = document.getElementById('author').value;
     let title = document.getElementById('title').value;
     let pages = document.getElementById('pages').value;
     let read = document.getElementById('read').checked;
     let newBook = new Book(author, title, pages, read);
-    Books.push(newBook);
+    let booksList = Books.find(item => {
+        if(item.title == title) return true;
+        else return false;
+    });   
+    if(booksList){
+        errorfield.textContent="Book already in Library";
+    }else{
+        Books.push(newBook);
     card.classList.remove('active');
     overlay.classList.remove('active');
-    newBook.createCard();
+    displayCards();
+    }
+
 })
 overlay.addEventListener('click',()=>{
     card.classList.remove('active');
     overlay.classList.remove('active');
 })
 function Book(author, title, pages, read){
-    this.author = author;
     this.title = title;
+    this.author = author;
     this.pages = pages;
     this.read = read;
 }
-Book.prototype.deleteBook = function(book){
-    target.parentElement.remove();
-}
 Book.prototype.createCard = function createCard(){
     let div = document.createElement('div');
+    let title= document.createElement('p');
+    let author= document.createElement('p');
+    let pages= document.createElement('p');
     let read;
     let clas;
+    let removeBtn= document.createElement('button');
+    let readBtn= document.createElement('button');
     div.classList.add('book-card');
     if(this.read){
         read ='Read';
@@ -55,19 +69,60 @@ Book.prototype.createCard = function createCard(){
         read="Not Read";
         clas= 'not-read';
     };
-    div.dataset.title=`${this.title}`;
-    div.innerHTML = `<p>${this.author} </p>
-    <p>${this.title} </p>
-    <p>${this.pages} </p>
-    <button data-readButton class='${clas}'>${read} </button>
-    <button data-removeButton class='remove'>Remove</button>`
+    title.textContent = this.title;
+    author.textContent = this.author;
+    pages.textContent = this.pages + " Pages";
+    readBtn.textContent = read;
+    removeBtn.textContent ='Remove';
+    readBtn.classList.add(clas);
+    removeBtn.classList.add('remove')
+    readBtn.onclick = readToggle;
+    removeBtn.onclick = remove;
+    div.dataset.title=this.title;
+    div.append(title);
+    div.append(author);
+    div.append(pages);
+    div.append(readBtn);
+    div.append(removeBtn);
     bookGrid.append(div);
 }
 
 function displayCards(){
-    Books.forEach(this.createCard());
+    resetGrid();
+    for(let book of Books){
+        book.createCard();
+    }
+}
+function resetGrid(){
+    bookGrid.innerHTML="";
 }
 
-document.querySelector('[data-removeButton]').addEventListener('click',(e)=>{
-    Book.deleteBook(e.target);
-})
+const readToggle = (e) => {
+    let title = e.target.parentNode.dataset.title;
+    let bookObject = Books.find(item => {
+        if(item.title == title) return true;
+        else return false;
+    });
+    if(bookObject.read==true){
+        bookObject.read= false;
+    }else{
+        bookObject.read = true;
+    }
+    displayCards();
+    console.log(bookObject);
+}
+const remove =(e)=>{
+    let title = e.target.parentNode.dataset.title;
+    let bookObject = Books.find(item => {
+        if(item.title == title) return true;
+        else return false;
+    });
+    console.log(bookObject);
+    let index= Books.findIndex(item =>{
+        if(item.title == title) return true
+    });
+    console.log(index);
+    Books.splice(index, 1);
+    delete bookObject;
+    displayCards();
+}
